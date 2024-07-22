35aa086b6cf3adf84eba27c7a10f183c9e8a4480
Eugene <eugene.voronovich@medi.report>
2024-07-23 01:46:58 +0200

    add initial autocomplete

--- src/app/routes/create-user-container/user-form/user-form.component.ts
+++ src/app/routes/create-user-container/user-form/user-form.component.ts
@@ -2,11 +2,14 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {CreateUserForm} from '../../../model/create-user-form';
 import {IsValidDirective} from '../../../shared/directives/is-valid.directive';
+import {Country} from '../../../shared/enum/country';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [ReactiveFormsModule, IsValidDirective],
+  imports: [ReactiveFormsModule, IsValidDirective, NgForOf, NgIf, AsyncPipe],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
@@ -17,4 +20,14 @@ export class UserFormComponent {
   public formIndex!: number;
   @Output()
   public removeForm: EventEmitter<number> = new EventEmitter<number>();
+
+
+  public filteredSuggestions = this.userForm?.get('country')?.valueChanges.pipe(
+    filter(v => !!v),
+    map(v => Object.values(Country).filter(c => c.includes(v ?? '')))
+  );
+
+  public listGroupItemPicked(country: string) {
+    this.userForm.get('country')?.setValue(country);
+  }
 }
